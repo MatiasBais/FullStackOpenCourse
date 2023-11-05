198ec824a4095e0b0abfdf3bb35acc54904e50c5
matiasbais <matiasbais1998@gmail.com>
2023-11-05 00:59:44 -0300

    first part

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
 const Part = (props) =>{
   return(
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -15,40 +15,59 @@ const Part = (props) =>{
 const Content = (props) =>{
   return(
     <div>
-      <p>
-        <Part part={props.part1} exercises={props.exercises1} />
-      </p>
-      <p>
-      <Part part={props.part2} exercises={props.exercises2} />
-      </p><p>
-      <Part part={props.part3} exercises={props.exercises3} />
-      </p>
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
     </div>
   )
 }
 
 const Total = (props) =>{
+  const total = (parts) =>{
+    let total = 0;
+    parts.forEach(element => {
+      total += element.exercises;
+    });
+    return total;
+  }
   return(
     <p>
-      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      Number of exercises {total(props.parts)}
     </p>
   )
 }
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Course = (props) =>{
+  return(
+    <div>
+      <Header course={props.course.name}/>
+      <Content parts={props.course.parts}/>
+      <Total parts={props.course.parts}/>
+    </div>
+  )
+}
 
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Course course={course} />
     </div>
   )
 }
